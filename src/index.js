76b0b3abfe8f4f0e70d266db7ad07e0478ebbff3
kalpshah485 <kalpshah485@gmail.com>
2022-03-03 15:05:41 +0530

    feat:Added Userlist Component.(Main Component)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+store.subscribe(() => console.log(store.getState()))
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,58 +1,76 @@
 var initialState = {
+    loading: true,
     users: [
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/1-image.jpg",
+            status: "Active",
+            Access: "Owner"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/2-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/3-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/4-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/5-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/6-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/7-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         },
         {
             first_name: 'John',
             last_name: 'Wick',
             email: 'john@example.com',
-            avatar: "https://reqres.in/img/faces/1-image.jpg"
+            avatar: "https://reqres.in/img/faces/8-image.jpg",
+            status: "Inactive",
+            Access: "Read"
         }
     ]
 };
 const reducer = (state=initialState,action) => {
     switch (action.type) {
         // case 'FETCH_USERS': {users: [...state.users,action.payload]}
+        // case 'LOADED': return {...state,loading: action.task}
         default: return state;
     }
 }
